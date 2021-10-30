76df18e28d586f9491ad37209bb5d7484d60277e
michaelalinks <56351494+michlinks@users.noreply.github.com>
2021-10-30 13:40:32 +0100

    Add files via upload

---
+++ cv_parser.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 30 09:33:13 2021
+
+@author: michaelalinks
+
+
+1. pip install pyresparser or pip3 install pyresparser
+2. pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-2.3.1/en_core_web_sm-2.3.1.tar.gz
+
+
+"""
+
+
+import nltk
+nltk.download('stopwords') # comment out after first run
+
+from pyresparser import ResumeParser
+data = ResumeParser('Michaela_Links_CV.pdf').get_extracted_data()
+
+print("\n\n",data)
+
+
+
+
+
