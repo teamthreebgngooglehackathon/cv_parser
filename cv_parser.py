c12ff8cd6d69bac87847fdee12dfb9be74aff781
michaelalinks <56351494+michlinks@users.noreply.github.com>
2021-10-30 21:18:14 +0100

    Update cv_parser.py
    docx to pdf conversion opens Word briefly, can be removed

--- cv_parser.py
+++ cv_parser.py
@@ -14,16 +14,35 @@ Created on Sat Oct 30 09:33:13 2021
 
 import json
 import nltk
+from docx2pdf import convert
+from pyresparser import ResumeParser
+import os
 
 # nltk.download('stopwords') # comment out after first run
 
-from pyresparser import ResumeParser
-data = ResumeParser('Michaela_Links_CV.pdf').get_extracted_data()
+file = "Michaela_Links_CV.pdf"
 
-# print("\n\n",data)
 
-json_object = json.dumps(data, indent = 4) 
-print("\n\n", json_object)
+if file.endswith(".pdf") is True: # for pdfs
+    try:
+        data = ResumeParser(file).get_extracted_data()
+        json_object = json.dumps(data, indent = 4) 
+        print("\n\n", json_object)
+    except FileNotFoundError:
+        print("File not found, try again")
+elif file.endswith(".docx") is True: # for docx -> pdf conversion
+    try: 
+        convert(file)
+        file_name = os.path.splitext(file)[0] + ".pdf"
+        data = ResumeParser(file_name).get_extracted_data()
+        json_object = json.dumps(data, indent = 4) 
+        print("\n\n", json_object)
+    except FileNotFoundError:
+        print("File not found, try again") 
+else:
+    print("Please attach a valid file type")
+
+
 
 
 
